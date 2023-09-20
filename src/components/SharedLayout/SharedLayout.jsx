3161cf23bd8fda50261bf1fe761650334c01520c
Liliia Paliichuk <113315152+lili2628@users.noreply.github.com>
2023-09-20 20:56:38 +0300

    update

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,16 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
-import { useDispatch } from 'react-redux';
 
 import AppBar from 'components/AppBar';
 import Spinner from 'components/Spinner';
-import { changeFiltered } from 'redux/cars/carsSlice';
 
 
 export default function SharedLayout() {
  
-
-
   return (
     <div>
       <AppBar/>

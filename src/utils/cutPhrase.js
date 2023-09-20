e1e9c44de62cd84ff858ddf6e4b92d5e541f8ee8
Liliia Paliichuk <113315152+lili2628@users.noreply.github.com>
2023-09-20 21:06:49 +0300

    update

--- src/utils/cutPhrase.js
+++ src/utils/cutPhrase.js
@@ -1,7 +1,7 @@
 export const cutPhrase = phrase => {
 
-  if (phrase.length > 20) {
-    const newPhrase = phrase.slice(0, 21);
+  if (phrase.length > 10) {
+    const newPhrase = phrase.slice(0, 11);
 
     return newPhrase;
 
